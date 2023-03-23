91d0047c96bf0fa57d2f0528d0cbe8e56da8c0a4
Guma <guma.bch@gmail.com>
2023-03-23 16:40:38 -0300

    aula-2

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -16,7 +16,9 @@ export class ListaLivrosComponent {
   ) { }
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca);
+    this.service.buscar(this.campoBusca).subscribe(
+      (retorno) =>  console.log(retorno)
+    );
   }
 }
 
